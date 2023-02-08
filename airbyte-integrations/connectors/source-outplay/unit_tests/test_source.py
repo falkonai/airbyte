c9c1820ed6e923ba73798d389fb99f5cfabdc29c
Saqib Rokadia <saqib@falkon.ai>
2023-02-07 16:18:40 -0800

    Fixes for unit tests and integration tests.

--- airbyte-integrations/connectors/source-outplay/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-outplay/unit_tests/test_source.py
@@ -4,10 +4,10 @@
 
 from unittest.mock import MagicMock
 
-from source_pardot.source import SourcePardot
+from source_outplay.source import SourceOutplay
 
 
 def test_check_connection(mocker, config):
-    source = SourcePardot()
+    source = SourceOutplay()
     logger_mock = MagicMock()
     assert source.check_connection(logger_mock, config) == (True, None)
